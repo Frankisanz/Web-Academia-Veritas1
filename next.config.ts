ffedf32752694cac5eec1327748e75a0939268b3
Fran <Amargued@gmail.com>
2026-04-27 20:49:48 +0200

    fix: actualizar basePath para coincidir con el nombre del repositorio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/AcademiaSoniaHigueras',
+  basePath: '/Web-Academia-Veritas1',
   images: {
     unoptimized: true,
   },
